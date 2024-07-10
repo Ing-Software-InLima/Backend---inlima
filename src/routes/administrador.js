ded8a596005cc289906f54448590fa6e42c9a861
RLJ17 <20201192@aloe.ulima.edu.pe>
2024-07-10 10:40:43 -0500

    Add: Municipalidad Name (Layout Component)

--- src/routes/administrador.js
+++ src/routes/administrador.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 import controller from '../controllers/administrador.js'
-
+import controller_ from '../controller/administrador.js'
 const routes = express.Router()
 
 routes.get('/', controller.findAll ) 
@@ -9,5 +9,5 @@ routes.post('/', controller.create )
 routes.get('/:id', controller.findOne )
 routes.put('/', controller.update )
 routes.delete('/:id', controller.remove)
-
+routes.post('/findMun', controller_.findMunicipalidadByAdminId)
 export default routes
